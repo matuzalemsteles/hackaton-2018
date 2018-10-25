4570cd6410f2beb985251ceb21f119e9b34a9302
oliveiraaraujo <112370525@liferay.com>
2018-10-25 17:31:56 -0300

    feat: add components

--- portal/axera-points/src/AppComponent.js
+++ portal/axera-points/src/AppComponent.js
@@ -1,6 +1,9 @@
 import {Config} from 'metal-state';
 import JSXComponent from 'metal-jsx';
 import withWeDeploy from './withWeDeploy';
+import PointsSummary from './components/points-summary/points-summary';
+import LatestPurchases from './components/latest-purchases/latest-purchases';
+import ProductsOffered from './components/products-offered/products-offered';
 
 class AppComponent extends JSXComponent {
     static PROPS = {
@@ -12,8 +15,20 @@ class AppComponent extends JSXComponent {
 
 	render() {
         return (
-            <div>
-				
+            <div class="container">
+				<div class="row">
+					<div class="col-2">
+						<PointsSummary />
+					</div>
+					<div class="col-10">
+						<div class="col-12">
+							<LatestPurchases />
+						</div>
+						<div class="col-12">
+							<ProductsOffered />
+						</div>
+					</div>
+				</div>
 			</div>
         );
     }
