16ec2a66933a3947841c414a75fe86ae8276ba91
Matuzalém Teles <matuzalemteles@gmail.com>
2018-10-25 17:15:52 -0300

    Add WeDeploy consumer

--- portal/axera-points/src/AppComponent.js
+++ portal/axera-points/src/AppComponent.js
@@ -1,40 +1,22 @@
-import core from 'metal';
+import {Config} from 'metal-state';
 import JSXComponent from 'metal-jsx';
+import withWeDeploy from './withWeDeploy';
 
-export default class AppComponent extends JSXComponent {
+class AppComponent extends JSXComponent {
+    static PROPS = {
+        portletNamespace: Config.string().value('(unknown portletNamespace)'),
+        contextPath: Config.string().value('(unknown contextPath)'),
+        portletElementId: Config.string().value('(unknown portletElementId)'),
+        data: Config.any(),
+    }
+
 	render() {
         return (
             <div>
-				<div>
-					<span class="tag">Portlet Namespace:</span> 
-					<span class="value">{this.props.portletNamespace}</span>
-				</div>
-
-				<div>
-					<span class="tag">Context Path:</span> 
-					<span class="value">{this.props.contextPath}</span>
-				</div>
-
-				<div>
-					<span class="tag">Portlet Element Id:</span>
-					<span class="value">{this.props.portletElementId}</span>
-				</div>
+				
 			</div>
         );
     }
 }
 
-AppComponent.PROPS = {
-    portletNamespace: {
-        validator: core.isString,
-        value: '(unknown portletNamespace)'
-    },
-	contextPath: {
-        validator: core.isString,
-        value: '(unknown contextPath)'
-    },
-	portletElementId: {
-		validator: core.isString,
-        value: '(unknown portletElementId)'
-	}
-};
+export default withWeDeploy(AppComponent);
