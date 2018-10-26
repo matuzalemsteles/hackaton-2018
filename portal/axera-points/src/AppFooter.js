26aadb5ffa51c200f3ee424f28db64a688bc4c65
oliveiraaraujo <112370525@liferay.com>
2018-10-26 13:20:03 -0300

    feat: adjusts in app footer

--- portal/axera-points/src/AppFooter.js
+++ portal/axera-points/src/AppFooter.js
@@ -1,22 +1,22 @@
 import JSXComponent from 'metal-jsx';
+//<h3>Our Company Brands</h3>
 
 export default class AppComponent extends JSXComponent {  
 	render() {
         return (
             <section>
-                <h3>Our Company Brands</h3>
                 <div class="d-flex p-2 justify-content-around">
                     <div class="d-inline-flex p-2 bd-highlight">
-                        <img src="/o/axera/images/asset-taf.png" class="img-fluid img-thumbnail" alt="Responsive image"/>
+                        <img src="/o/axera/images/asset-taf.png" style="height:120px" class="img-fluid img-thumbnail" alt="Responsive image"/>
                     </div>
                     <div class="d-inline-flex p-2 bd-highlight">
-                        <img src="/o/axera/images/asset-gnc.png" class="img-fluid img-thumbnail" alt="Responsive image"/>
+                        <img src="/o/axera/images/asset-gnc.png" style="height:120px" class="img-fluid img-thumbnail" alt="Responsive image"/>
                     </div>
                     <div class="d-inline-flex p-2 bd-highlight">
-                        <img src="/o/axera/images/asset-maxiva.png" class="img-fluid img-thumbnail" alt="Responsive image"/>
+                        <img src="/o/axera/images/asset-maxiva.png" style="height:120px" class="img-fluid img-thumbnail" alt="Responsive image"/>
                     </div>
                     <div class="d-inline-flex p-2 bd-highlight">
-                        <img src="/o/axera/images/asset-truekids.png" class="img-fluid img-thumbnail" alt="Responsive image"/>
+                        <img src="/o/axera/images/asset-truekids.png" style="height:120px" class="img-fluid img-thumbnail" alt="Responsive image"/>
                     </div>
                 </div>                
             </section>
